23187abc83005593d8b305488471ec8e3c51dd61
Pepijn Kenter <titusjan@gmail.com>
2022-04-03 16:51:58 +0200

    Initial set of test data.
    Takes about 15 minutes my apple

---
+++ data/hdf5/my-tests/readme.txt
@@ -0,0 +1,2 @@
+Data created by the create_test_data.py script in the Argos git repo.
+
---
+++ data/img/read.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+fileName = "vantablack-s-vis-spray-1.jpg"
+
+with Image.open(fileName) as image:
+    arr = 
